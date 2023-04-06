56db02fa3b9288bb1ba9896843028039f5760ade
almanza1112 <almanza1112@gmail.com>
2023-04-06 02:41:00 -0400

    added faceUpCard

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ refPlayers.on('value', async (snapshot) => {
         // Retrieve how many players in table
         var numOfPlayers = Object.keys(data).length;
 
-        if (numOfPlayers > 1) {
+        if (numOfPlayers > 1) {            
             // Check if round is in progress
             var result = await isRoundInProgress();
             if (!result) {
@@ -46,7 +46,8 @@ refPlayers.on('value', async (snapshot) => {
 
  async function isRoundInProgress(){
      var result = await refIsRoundInProgress.once('value');
-     return result.val();
+     //return result.val();
+     return false;
     
 }
 
@@ -66,6 +67,8 @@ function startGame(data , numOfPlayers) {
 
     // Set what the remaining cards are to the dealer
     cardUpdates['dealer'] = _startingHand['deck'];
+    cardUpdates['faceUpCard'] = _startingHand['faceUpCard'];
+    
     // Update roundInProgress to true
     update = { "roundInProgress": true, "cards": cardUpdates }
     
@@ -93,7 +96,7 @@ const signInRouter = require('./routes/sign_in');
 app.use('/sign_in', signInRouter)
 
 //Uncomment below for local testing
-//app.listen(3000, () => console.log('Server Started'))
+app.listen(3000, () => console.log('Server Started'))
 
 //Uncomment below for push
-app.listen(process.env.PORT || 5000 , () => console.log('Server Started'))
+//app.listen(process.env.PORT || 5000 , () => console.log('Server Started'))
